c6771a88ed5acf9beb57f02258707f4f454e78a7
Bernie Ramone <makio1981@hotmail.com>
2022-09-18 10:42:16 -0500

    new end points

--- PlatformService/PlatformService.API/Controllers/PlatformsController.cs
+++ PlatformService/PlatformService.API/Controllers/PlatformsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.API.Data;
 using PlatformService.API.Dtos;
+using PlatformService.API.Models;
 
 namespace PlatformService.API.Controllers
 {
@@ -28,8 +29,32 @@ namespace PlatformService.API.Controllers
             var platformItem = _repository.GetPlatforms();
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
 
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
 
+            if (platformItem is null)
+            {
+                return NotFound();
+            }
+
+            var mapp = _mapper.Map<PlatformReadDto>(platformItem);
+            return Ok(mapp);
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreate)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreate);
+            _repository.CreatePlatform(platformModel);
+            var response = _repository.SaveChanges();
+
+            var platformResponse = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute( nameof(GetPlatformById), new { Id = platformResponse.Id} , response);
         }
     }
 }
